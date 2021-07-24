073d20cfcbe698f871fbbab5192de9534a39e2eb
prakharb10 <friday15051@gmail.com>
2021-07-24 20:01:22 +0530

    code fixes

--- QRPG.py
+++ QRPG.py
@@ -19,7 +19,7 @@ backend_choice = st.radio('Choose a backend', ('Simulator', 'IBMQ Device'))
 @st.cache(suppress_st_warning=True)
 def loadAccount(token):
     try:
-        IBMQ.enable_account(account_token)
+        IBMQ.enable_account(token)
     except IBMQAccountError:
         try:
             IBMQ.load_account()
@@ -33,11 +33,13 @@ def loadAccount(token):
         st.success('Loaded account successfully!')
 
 
-if(backend_choice == 'IBMQ Device'):
-    st.info('To run the program on a real Quantum Computer, you have to enter your IBMQ Account Token which can be found on the Account Page of your IBM Quantum Experience Account.')
+if backend_choice == 'IBMQ Device':
+    st.info(
+        'To run the program on a real Quantum Computer, you have to enter your IBMQ Account Token which can be found on the Account Page of your IBM Quantum Experience Account.')
     account_token = st.text_input('IBMQ Account Token')
-    st.info('NOTE: Running the task on a real quantum computer may take upto several minutes depending on the job queue')
-    if(account_token != ""):
+    st.info(
+        'NOTE: Running the task on a real quantum computer may take upto several minutes depending on the job queue')
+    if account_token != "":
         loadAccount(account_token)
 
 pass_length = st.slider('Password Length', min_value=4,
@@ -48,14 +50,14 @@ clicked = st.button("Generate")
 def getBackend():
     provider = IBMQ.get_provider(hub='ibm-q')
     backendd = least_busy(provider.backends(filters=lambda x: x.configuration().n_qubits >= 5
-                                            and not x.configuration().simulator
-                                            and x.status().operational == True))
+                                                              and not x.configuration().simulator
+                                                              and x.status().operational == True))
     st.write('Backend Chosen: ', backendd.name())
     return backendd
 
 
+# TODO: try caching this
 with st.echo():
-
     def circuit():
         q = QuantumRegister(5, 'q')
         circuit = QuantumCircuit(q)
@@ -64,13 +66,20 @@ with st.echo():
         return circuit
 
 
+def job_state_print(job_id, job_status, job, queue_info):
+    print(job_status)
+    st.write(job_status)
+
+
+# TODO: Show job status
 def run_job(circ, sht):
-    if(backend_choice == 'IBMQ Device'):
+    if backend_choice == 'IBMQ Device':
         bk = getBackend()
     else:
         bk = Aer.get_backend('qasm_simulator')
     job = execute(circ, backend=bk, shots=int(sht))
-    job_monitor(job)
+    job.wait_for_final_state(callback=job_state_print)
+    # job_monitor(job)
     counts = job.result().get_counts()
     ccounts = counts.copy()
     for i in list(counts):
@@ -85,13 +94,13 @@ def bintoASCII(count):
     password = ''
     for i in list(count):
         for j in range(count[i]):
-            if(int(str(i)[-2:], 2) == 1):
+            if int(str(i)[-2:], 2) == 1:
                 num = int(str(i), 2) + 32
-                if(num == 32):
+                if num == 32:
                     num += 1
-            elif (int(str(i)[-2:], 2) == 3):
+            elif int(str(i)[-2:], 2) == 3:
                 num = int(str(i), 2) + 96
-                if(num == 127):
+                if num == 127:
                     num -= 1
             else:
                 num = int(str(i), 2) + 64
@@ -100,7 +109,7 @@ def bintoASCII(count):
     st.title(password)
 
 
-if (clicked):
+if clicked:
     c = circuit()
     st.write(c.draw(output='mpl', interactive=True))
     st.caption('Quantum Circuit')
